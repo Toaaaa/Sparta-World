3d1afefea041e5580f91481f0eeb6cbafe34c929
kim minjun <k980128@naver.com>
2025-02-17 15:46:36 +0900

    feat : Player interaction added (Raycasting)

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,14 +13,18 @@ public class Player : MonoBehaviour
     float verti_time;// v인풋 지속시간
     float hori_delta;
     float verti_delta;
-    Vector2 dirVec;// 바라보는 방향
     public float speed = 5f;
     public bool isMoving;
     public bool cantAction;
 
+    //플레이어 상호작용
+    Vector2 dirVec;// 바라보는 방향
+    GameObject scannedObject;// dirVec 방향으로 스캔한 오브젝트
+
     //플레이어 컴포넌트
     private Rigidbody2D rb;
     private Animator anim;
+
 
     void Start()
     {
@@ -31,15 +35,21 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+        InteractScan();
     }
 
     private void FixedUpdate()
     {
+        //플레이어 이동
         Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
         rb.velocity = moveVec * speed;
+        //레이캐스팅
+        //Debug.DrawRay(this.transform.position, dirVec*1.7f, new Color(0, 1, 0));// hit 판정 레이를 보여주는 디버깅용 코드.
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dirVec, 0.7f, LayerMask.GetMask("Interactable"));
+        scannedObject = hit.collider != null ? hit.collider.gameObject : null;
     }
 
-    void PlayerMovement()//플레이어의 방향값 설정 (마지막에 입력된 값을 출력 및 한쪽 Direction으로만 힘이 작용하게 제한)
+    void PlayerMovement()// 플레이어의 방향값 설정 (마지막에 입력된 값을 출력 및 한쪽 Direction으로만 힘이 작용하게 제한)
     {
         isMoving = h != 0 || v != 0;// h: -1 왼쪽, +1 오른쪽  v: -1 아래, +1 위
         if (hori_time != 0 && verti_time != 0)//만약 두개가 동시에 눌러지고 있을 경우 마지막에 입력된 값을 출력하기
@@ -84,11 +94,24 @@ public class Player : MonoBehaviour
         anim.SetFloat("Vertical", v);
         anim.SetBool("isMoving", isMoving);
     }
-
-    void CheckTheLastDir()//방향 인풋 h,v를 당담하는 입력중 동시에 입력중일떄 마지막으로 입력된 인풋을 우선으로 제공하는 함수.
+    void CheckTheLastDir()// 방향 인풋 h,v를 당담하는 입력중 동시에 입력중일떄 마지막으로 입력된 인풋을 우선으로 제공하는 함수.
     {
         bool lastHorizontal = hori_time > verti_time;
         h = lastHorizontal ? (cantAction ? 0 : Input.GetAxisRaw("Horizontal")) : 0;
         v = lastHorizontal ? 0 : (cantAction ? 0 : Input.GetAxisRaw("Vertical"));
+    }
+    void InteractScan()// 스캔된 오브젝트와 상호작용.
+    {
+        if(Input.GetButtonDown("Jump") && scannedObject != null)
+        {
+            if(scannedObject.tag == "Game1")
+            {
+                Debug.Log("Game1 시작");
+            }
+            if(scannedObject.tag == "Game2")
+            {
+                Debug.Log("Game2 시작");
+            }
+        }
     }
 }
