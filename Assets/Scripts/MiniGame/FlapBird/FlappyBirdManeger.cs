eb95b219e5b4355eb5e6be0262eb359d81d8225a
kim minjun <k980128@naver.com>
2025-02-18 12:50:53 +0900

    feat : Result page.

--- Assets/Scripts/MiniGame/FlapBird/FlappyBirdManeger.cs
+++ Assets/Scripts/MiniGame/FlapBird/FlappyBirdManeger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class FlappyBirdManeger : MonoBehaviour
@@ -10,15 +11,34 @@ public class FlappyBirdManeger : MonoBehaviour
     public bool StartGame = false;
     public int Score = 0;
 
+    public GameObject ResultUI;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI resultScoreText;
+
     private void Start()
     {
         StartCoroutine(StartGameCoroutine());
     }
 
+    private void Update()
+    {
+        scoreText.text =  "SCORE : " + Score.ToString();
+        resultScoreText.text = "SCORE : " + Score.ToString();
+    }
+
+    private void OnEnable()
+    {
+        //처음에는 playerSpaceToStart 텍스트 추가
+    }
+    private void OnDisable()
+    {
+        ResultUI.SetActive(false);
+    }
     public void StopGame()// 게임 오버.
     {
         pipeSpawner.StopPipe();
         StartGame = false;
+        ResultUI.SetActive(true);
     }
 
 
