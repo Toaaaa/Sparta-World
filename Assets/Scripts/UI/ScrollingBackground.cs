5645da851baa26382d7b0190b9a0a95929732864
kim minjun <k980128@naver.com>
2025-02-18 21:21:19 +0900

    text : simple variable text change

--- Assets/Scripts/UI/ScrollingBackground.cs
+++ Assets/Scripts/UI/ScrollingBackground.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 public class ScrollingBackground : MonoBehaviour
 {
     [SerializeField] private RawImage _img;
-    public float _x, _y;// x, y축으로 얼마나 움직일지 결정.
+    public float xspd, yspd;// x, y축으로 얼마나 움직일지 결정.
 
     void Update()
     {
-        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(xspd, yspd) * Time.deltaTime, _img.uvRect.size);
     }
 }
