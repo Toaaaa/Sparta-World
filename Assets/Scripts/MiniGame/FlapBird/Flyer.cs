4f1dd3bc2c6bf3aa25d49e0ccb4f08ce15fb1f17
kim minjun <k980128@naver.com>
2025-02-18 11:18:34 +0900

    feat : Flyer gravity and fly added.

--- Assets/Scripts/MiniGame/FlapBird/Flyer.cs
+++ Assets/Scripts/MiniGame/FlapBird/Flyer.cs
@@ -1,18 +1,38 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Flyer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    RectTransform rect;
+    [SerializeField] float jumpPower = 110f;
+    [SerializeField] float jumpTime = 0.2f;
+    [SerializeField] float gravityValue = 350;
+
+    private void Awake()
     {
-        
+        rect = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (GameManager.Instance.flappyBirdManeger.StartGame)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                FlyUp();
+            }
+            GravityDown();
+        }
+    }
+    void FlyUp()
+    {
+        rect.DOAnchorPosY(rect.anchoredPosition.y + jumpPower, jumpTime)
+            .SetEase(Ease.OutQuad);
+    }
+    void GravityDown()
+    {
+        rect.anchoredPosition += Vector2.down * gravityValue * Time.deltaTime;
     }
 }
